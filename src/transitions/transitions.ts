e208b110e97053aa9d116b5317bfd974960f5d43
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-06-02 11:15:33 +0200

    orm for transitions excercise

--- src/transitions/transitions.ts
+++ src/transitions/transitions.ts
@@ -1,2 +1,20 @@
 import './../style.scss';
 import './transitions.scss';
+import './form.scss';
+
+var button = document.getElementsByClassName('ui-button')[0];
+button.addEventListener('click', handleEvent);
+
+function handleEvent(e) {
+  console.log('handling event: ', e);
+
+  switch (e.type) {
+    case 'click':
+      setState('subscribing');
+      break;
+  }
+}
+
+function setState(state) {
+  console.log('button clicked, new state is:', state);
+}
