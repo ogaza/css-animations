3d836d7e77f4529301d47ece2a6e31f35d4def1a
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-06-05 08:55:59 +0200

    keyframes: form slide in, invalid input shake, ball pause its move on hover

--- src/keyframes/keyframes.ts
+++ src/keyframes/keyframes.ts
@@ -1,6 +1,7 @@
 // import './../style.scss';
 import './ball.scss';
 import './keyframes.scss';
+import './form-animations.scss';
 
 var button = document.getElementsByClassName('ui-button')[0];
 button.addEventListener('click', handleEvent);
