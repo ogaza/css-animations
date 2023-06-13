0c7684c7a9fb5e6e09acbae023dd3b1bec0433fb
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-06-13 11:32:40 +0200

    states: ripple

--- src/states/ripple.ts
+++ src/states/ripple.ts
@@ -1,33 +1,21 @@
-// var button = document.querySelector<HTMLElement>('.button');
-// button.addEventListener('mousedown', handleMouseDown);
-// button.addEventListener('touchstart', handleTouchStart);
-
 var rippledEl = document.querySelector<HTMLElement>('.ripple');
-rippledEl.addEventListener('click', ripple);
+rippledEl.addEventListener('mousedown', handleMouseDown);
+rippledEl.addEventListener('touchstart', handleTouchStart);
 
-// function handleMouseDown(e) {
-//   const x = e.clientX;
-//   const y = e.clientY;
-//   const boundingClientRect = button.getBoundingClientRect();
-
-//   button.style.setProperty('--x', `${x - boundingClientRect.left}px`);
-//   button.style.setProperty('--y', `${y - boundingClientRect.top}px`);
-// }
-
-// function handleTouchStart(e) {
-//   const x = e.touches[0].clientX;
-//   const y = e.touches[0].clientY;
-//   const boundingClientRect = button.getBoundingClientRect();
-
-//   button.style.setProperty('--x', `${x - boundingClientRect.left}px`);
-//   button.style.setProperty('--y', `${y - boundingClientRect.top}px`);
-// }
-
-function ripple(e) {
+function handleMouseDown(e) {
   const x = e.clientX;
   const y = e.clientY;
   const boundingClientRect = rippledEl.getBoundingClientRect();
 
-  rippledEl.style.setProperty('--y', `${y - boundingClientRect.top}px`);
   rippledEl.style.setProperty('--x', `${x - boundingClientRect.left}px`);
+  rippledEl.style.setProperty('--y', `${y - boundingClientRect.top}px`);
+}
+
+function handleTouchStart(e) {
+  const x = e.touches[0].clientX;
+  const y = e.touches[0].clientY;
+  const boundingClientRect = rippledEl.getBoundingClientRect();
+
+  rippledEl.style.setProperty('--x', `${x - boundingClientRect.left}px`);
+  rippledEl.style.setProperty('--y', `${y - boundingClientRect.top}px`);
 }
