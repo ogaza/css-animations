bef8e54bcda2b7cf00be374debeffaad4726cdb3
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-06-13 13:18:19 +0200

    styling organization

--- src/fundamentals/index.ts
+++ src/fundamentals/index.ts
@@ -1,8 +1,2 @@
 import './../styles/base.scss';
 import './fundamentals.scss';
-
-document.body.addEventListener('click', handleClick);
-
-function handleClick(e) {
-  document.body.style.setProperty('--bg-color', 'rgb(203, 204, 142)');
-}
