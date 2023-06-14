d189babe6fbb7a0b32da2a4fb48b1019b505f15b
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-06-14 13:34:29 +0200

    ripple: ripple efect using the after pseudo element

--- src/ripple/ripple.ts
+++ src/ripple/ripple.ts
@@ -1,21 +1,53 @@
 var rippledEl = document.querySelector<HTMLElement>('.ripple');
-rippledEl.addEventListener('mousedown', handleMouseDown);
-rippledEl.addEventListener('touchstart', handleTouchStart);
+rippledEl.addEventListener('mousedown', getHandleMouseDown(rippledEl));
+rippledEl.addEventListener('touchstart', getHandleTouchStart(rippledEl));
 
-function handleMouseDown(e) {
-  const x = e.clientX;
-  const y = e.clientY;
-  const boundingClientRect = rippledEl.getBoundingClientRect();
+function getHandleMouseDown(htmlElement) {
+  return function handleMouseDown(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    const boundingClientRect = htmlElement.getBoundingClientRect();
 
-  rippledEl.style.setProperty('--x', `${x - boundingClientRect.left}px`);
-  rippledEl.style.setProperty('--y', `${y - boundingClientRect.top}px`);
+    htmlElement.style.setProperty('--x', `${x - boundingClientRect.left}px`);
+    htmlElement.style.setProperty('--y', `${y - boundingClientRect.top}px`);
+  };
 }
 
-function handleTouchStart(e) {
-  const x = e.touches[0].clientX;
-  const y = e.touches[0].clientY;
-  const boundingClientRect = rippledEl.getBoundingClientRect();
+function getHandleTouchStart(htmlElement) {
+  return function handleTouchStart(e) {
+    const x = e.touches[0].clientX;
+    const y = e.touches[0].clientY;
+    const boundingClientRect = htmlElement.getBoundingClientRect();
 
-  rippledEl.style.setProperty('--x', `${x - boundingClientRect.left}px`);
-  rippledEl.style.setProperty('--y', `${y - boundingClientRect.top}px`);
+    htmlElement.style.setProperty('--x', `${x - boundingClientRect.left}px`);
+    htmlElement.style.setProperty('--y', `${y - boundingClientRect.top}px`);
+  };
+}
+
+var rippledEl2 = document.querySelector<HTMLElement>('.ripple-using-after');
+
+rippledEl2.addEventListener('mousedown', handleEvent);
+rippledEl2.addEventListener('mousedown', getHandleMouseDown(rippledEl2));
+rippledEl2.addEventListener('mouseup', handleEvent);
+rippledEl2.addEventListener('touchstart', handleEvent);
+rippledEl2.addEventListener('touchstart', getHandleTouchStart(rippledEl2));
+rippledEl2.addEventListener('touchend', handleEvent);
+rippledEl2.addEventListener('click', handleEvent);
+
+export function handleEvent(e) {
+  switch (e.type) {
+    case 'mousedown':
+    case 'touchstart':
+      rippledEl2.dataset.state = 'pressed';
+      break;
+    case 'mouseup':
+    case 'touchend':
+      rippledEl2.removeAttribute('data-state');
+      break;
+    case 'click':
+      rippledEl2.dataset.state = 'clicked';
+      break;
+    default:
+      break;
+  }
 }
